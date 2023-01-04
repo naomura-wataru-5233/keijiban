e70950e21664acea1ce84080533414b2737515b5
a3168330 <a3168330@mi.sanno.ac.jp>
2023-01-04 14:25:08 +0900

    keijiban

---
+++ 4each_keijiban/insert.php
@@ -0,0 +1,17 @@
+<?php
+
+mb_internal_encoding("utf8");
+$pdo=new PDO("mysql:dbname=lesson01;host=localhost;","root","");
+$pdo->exec("insert into 4each_keijiban(handlename,title,comments)
+values('".$_POST['handlename']."','".$_POST['title']."','".$_POST['comments']."');");
+header("Location:http://localhost/4each_keijiban/index.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>提出課題</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
